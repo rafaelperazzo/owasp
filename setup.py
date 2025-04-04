1230f932786cb2199dd10b84a8c2b024a817afc3
Rafael Perazzo <rafaelperazzo@gmail.com>
2025-04-04 10:23:05 -0300

    fix: setup.py - corrigidas dependencias - tentando fazer funcionar 2

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 from cripto import __version__ as version
 
+with open('requirements.txt', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
 setup(     
      name="cripto-tools",     
      version=version,
      python_requires=">=3.6",   
      py_modules=["cripto"],
      author="Rafael Perazzo",
-     install_requires=['pycryptodome','argon2-cffi','python-gnupg'],
+     install_requires=requirements,
 )
